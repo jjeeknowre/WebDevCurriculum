cb88747dd52239e2e29f5608e83149bd3354e439
Joshua Jee - XPS15 <jjee@knowre.com>
2018-10-16 10:28:46 +0900

    Quest 12 completed

--- Part 3. node.js/Quest 12. AJAX/skeleton/server.js
+++ Part 3. node.js/Quest 12. AJAX/skeleton/server.js
@@ -1,15 +1,102 @@
 const express = require('express'),
 	path = require('path'),
-	app = express();
+	app = express()
+	EventEmitter = require('events')
+	fs = require('fs');
+const emitter = new EventEmitter();
 
 app.use(express.static('client'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
+	console.log('test');
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-/* TODO: 여기에 처리해야 할 요청의 주소별로 동작을 채워넣어 보세요..! */
+
+// from Quest 11
+app.get('/foo', (req, res) => {
+	let message = 'Hello World';
+	if (req.query.bar) {
+		message = `Hello, ${req.query.bar}`;
+	}
+	console.log(message);
+	res.send(message);
+});
+app.get('/foo/:bar', (req, res) => { 
+	let message = `Hello, ${req.params.bar}`;
+	console.log(message);
+	res.send(message);
+});
+app.post('/foo', (res) => {
+	let message = 'Hello World';
+	if (req.query.bar) {
+		message = `Hello, ${req.query.bar}`;
+	}
+	console.log(message);
+	res.send(message);
+});
+
+
+// Notepad API
+let filedir = './client/filedir'; // code doesn't work when directory is named 'files' (same as API)
+app.get('/files', (req, res) => {
+	fs.readdir(filedir, 'utf8', (err, files) => {
+		res.send(files)
+		//res.send(JSON.stringify(files));
+	});
+});
+app.get('/files/:filename', (req, res) => {
+	let filepath = filedir + '/' + req.params.filename;
+	fs.stat(filepath, (err) => {
+		if (!err) {
+			fs.readFile(filepath, 'utf8', (err, text) => res.send(text));
+		} else {
+			res.send('no such file exists');
+		}
+	});
+});
+app.post('/files', (req, res) => {
+	let filepath = filedir + '/' + req.body.name;
+	console.log(req.body);
+	fs.stat(filepath, (err) => {
+		if (!err) {
+			res.send(`the file ${req.body.name} already exists`);
+		} else {
+			fs.writeFile(filepath, req.body.text, 'utf8', (err) => {
+				res.send(`the file ${req.body.name} was created`);
+			});
+		}
+	});
+});
+app.put('/files/:filename', (req, res) => {
+	let filepath = filedir + '/' + req.params.filename;
+	fs.stat(filepath, (err) => {
+		if (!err) {
+			fs.writeFile(filepath, req.body.text, 'utf8', (err) => {
+				res.send(`the file ${req.params.filename} was saved`);
+			});
+		} else {
+			res.send(`the file ${req.body.name} does not exist`);
+		}
+	});
+});
+app.delete('/files/:filename', (req, res) => {
+	let filepath = filedir + '/' + req.params.filename;
+	fs.stat(filepath, (err) => {
+		if (!err) {
+			fs.unlink(filepath, req.body.text, 'utf8', (err) => {
+				res.send(`the file ${req.params.filename} was deleted`);
+			});
+		} else {
+			res.send(`the file ${req.body.name} does not exist`);
+		}
+	});
+});
+
 
 const server = app.listen(8080, () => {
 	console.log('Server started!');
 });
+
